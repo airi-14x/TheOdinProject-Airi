597020edf0e129c77cca6e9bdf6753aa4b3e45b0
airi-14x <airi.14x@gmail.com>
2018-07-04 19:18:24 -0400

    Need to fix repeat function

--- learn_ruby-master/03_simon_says/simon_says.rb
+++ learn_ruby-master/03_simon_says/simon_says.rb
@@ -1,1 +1,16 @@
 #write your code here
+
+def echo(message)
+    message
+end
+
+def shout(message)
+    message.upcase
+end
+
+def repeat(message, num = 2)
+
+    num.times do
+      message
+    end
+end
