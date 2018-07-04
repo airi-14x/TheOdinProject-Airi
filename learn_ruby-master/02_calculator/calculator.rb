cbb087a74efb2b085bc401bc06ee42d62249f9c5
airi-14x <airi.14x@gmail.com>
2018-07-04 18:58:45 -0400

    Updating the Ruby

--- learn_ruby-master/02_calculator/calculator.rb
+++ learn_ruby-master/02_calculator/calculator.rb
@@ -1,1 +1,17 @@
 #write your code here
+
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(arr)
+  sum = 0
+  for i in arr
+    sum += i
+  end
+  return sum
+end
