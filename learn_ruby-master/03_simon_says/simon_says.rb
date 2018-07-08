48bb1c13dae08b130c7f5b3e6f63bae2728e3f21
airi-14x <airi.14x@gmail.com>
2018-07-08 15:01:53 -0400

    Completed Simon says

--- learn_ruby-master/03_simon_says/simon_says.rb
+++ learn_ruby-master/03_simon_says/simon_says.rb
@@ -12,11 +12,33 @@ def repeat(message, num = 2)
 if num == 2
       message + " " + message
 else
-    i = 0
-    while(i < num) do
-      message = message + " "
-      i = i + 1
-    end
-    return message
+    ([message] * num).join(" ")
 end
+end
+
+def start_of_word(message, up_to_index)
+message[0 ... up_to_index]
+end
+
+def first_word(message)
+new_message = message.split(" ")
+new_message[0]
+end
+
+def titleize(message)
+little_words = ["and", "the", "over", "or"]
+new_message = message.split(" ")
+output_message = []
+i = 0
+new_message.each do |item|
+  if i == 0 || i == new_message.length - 1 || item.length > 4
+    output_message.push(item.capitalize)
+  else
+    output_message.push(item)
+  end
+  i += 1
+end
+output_message.join(" ")   
+#message.split(" ").collect(&:capitalize).join(" ")
+
 end
