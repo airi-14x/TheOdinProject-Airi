d72ba6a6907e4a527772866d7e0da4bf9dc15f8f
airi-14x <airi.14x@gmail.com>
2018-07-13 13:26:11 -0400

    Pig Latin is finally done. Wow, it took some time....

--- learn_ruby-master/04_pig_latin/pig_latin.rb
+++ learn_ruby-master/04_pig_latin/pig_latin.rb
@@ -1,7 +1,7 @@
 #write your code here
 def translate(words)
 
-vowels = ['a', 'e', 'o', 'i', 'qu', 'u']
+vowels = ['a', 'e', 'o', 'i', 'qu']
 store_consonants = []
 word_index = 0
 one_vowel = 0 # Did we get a vowel yet?
@@ -10,28 +10,28 @@ words_arr = words.split("")
 more_than_one = false # Is there more than one word to translate?
 
 if words.include? " "
-  puts "There's more than one word"
+  #puts "There's more than one word"
   more_words_arr = words.split(" ")
-  puts more_words_arr
+  #puts more_words_arr
   more_than_one = true
   num_words = more_words_arr.length
 end
 
 if more_than_one == false
 words_arr.each do |letter|
-    puts letter
+    #puts letter
     if vowels.include? letter
-      puts "Here???!"
+      #puts "Here???!"
       one_vowel = 1
       break
     elsif one_vowel == 1
-        puts "Here!"
-        puts letter
+        #puts "Here!"
+        #puts letter
         break
     else
       # Element to remove and the index that it's at.
-      puts "IN LOOP!"
-      puts letter
+      #puts "IN LOOP!"
+      #puts letter
       store_consonants.push(letter)
       upto_delete_index = words_arr.index(letter)
     end
@@ -65,6 +65,7 @@ else
         one_vowel = 1
         break
       elsif one_vowel == 1
+        #puts store_consonants
         break
       else
         store_consonants.push(letter)
@@ -74,24 +75,41 @@ else
 
       # Format the word #
       if store_consonants.empty? # Nothing was added, first "if"
-        current_word_letters << "ay"
-        current_word_letters.join("") # Joining letters
+        words_arr[current_word_index] << "ay"
+        #puts words_arr[current_word_index]
       else
-        store_consonants << "ay"
-        store_consonants.join
-        current_word_letters = current_word_letters.slice(upto_delete_index+1 .. -1)
-        current_word_letters = current_word_letters + store_consonants
-        current_word_letters.join
+        #store_consonants << "ay"
+
+        # Get rest everything that comes after the consonant #
+        word = words_arr[current_word_index]
+        consonant = word.slice(0 .. upto_delete_index)
+        rest = word.slice(upto_delete_index + 1 .. -1)
+        consonant = consonant + "ay"
+        rest = rest + consonant
+        #puts rest
+        words_arr[current_word_index] = rest
+
       end
-      new_words_arr[current_word_index] = current_word_letters
       current_word_index += 1 # Increment counter
       one_vowel = 0 #Reset
       store_consonants.clear
     end
-    puts new_words_arr[0]
-    puts "UHHHH"
-    puts new_words_arr[1]
 
+    words = ""
+    word_index = 0
+
+    while word_index + 1 < num_words do
+      words += words_arr[word_index] + " " + words_arr[word_index + 1]
+      word_index += 2
+
+      if word_index + 1 < num_words #If we are going to go again through this loop, we need the space
+        words += " "
+      end
+      #if words_index > num_words
+         #words.chomp()
+      #end
+    end
+    return words
 end
 
 end
