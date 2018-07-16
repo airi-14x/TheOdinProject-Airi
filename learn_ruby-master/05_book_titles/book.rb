703b821c2d400b5a141c5ebf9b45405450782054
airi-14x <airi.14x@gmail.com>
2018-07-16 12:24:58 -0400

    Working on titlising function

--- learn_ruby-master/05_book_titles/book.rb
+++ learn_ruby-master/05_book_titles/book.rb
@@ -1,3 +1,27 @@
 class Book
-# write your code here
+  def initialize
+    # Current Book default constructor
+  end
+
+  attr_reader :title # Getter
+
+  def title=(_string)
+    @title = title_function(_string)
+  end
+
+  def title_function(_string)
+    final_string = []
+    current_arr = _string.split(' ')
+    special_words = %w[the a an and in the of]
+    first_word = false
+    index = 0
+    while index < current_arr.length
+      if index == 0
+        if current_arr[0].include? special_words
+        end
+      end
+
+      index += 1
+    end
+  end
 end
