465ddbbabc3f316333e8850eb746fad4e47e02d7
airi-14x <airi.14x@gmail.com>
2018-07-16 14:29:25 -0400

    Completed book titles

--- learn_ruby-master/05_book_titles/book.rb
+++ learn_ruby-master/05_book_titles/book.rb
@@ -1,7 +1,4 @@
 class Book
-  def initialize
-    # Current Book default constructor
-  end
 
   attr_reader :title # Getter
 
@@ -11,17 +8,51 @@ class Book
 
   def title_function(_string)
     final_string = []
+    new_string = ""
     current_arr = _string.split(' ')
-    special_words = %w[the a an and in the of]
+    special_words = Set["the", "a", "an", "and", "in", "the", "of"]
     first_word = false
     index = 0
+    #puts "*******"
+    #puts current_arr
+    #puts current_arr.length
+    #puts special_words.include? current_arr[0]
+    #puts !(special_words.include? current_arr[1])
     while index < current_arr.length
+      #puts index
       if index == 0
-        if current_arr[0].include? special_words
+        if special_words.include? current_arr[0]
+          if !(special_words.include? current_arr[1]) == true # Doesn't contain special word
+            first_word = true
+          end
         end
       end
 
+      if (first_word == true)
+        final_string << current_arr[0].capitalize
+        final_string << current_arr[1].capitalize
+        index = 1
+        first_word = false # Finished adding the first two words
+      else
+        if (special_words.include? current_arr[index]) == false
+          final_string << current_arr[index].capitalize
+        else
+          final_string << current_arr[index]
+        end
+      end
+      #puts index
       index += 1
     end
+
+    index = 0
+    while index < final_string.length
+      new_string += final_string[index]
+
+      if index + 1 < final_string.length
+        new_string += " "
+      end
+      index += 1
+    end
+    return new_string
   end
 end
