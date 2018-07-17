5992be2be566458b97e736a0e4aee833d891eb03
airi-14x <airi.14x@gmail.com>
2018-07-17 10:59:56 -0400

    Completed Timer

--- learn_ruby-master/06_timer/timer.rb
+++ learn_ruby-master/06_timer/timer.rb
@@ -1,3 +1,34 @@
 class Timer
-  #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds = 0
+  end
+
+  def seconds=(_seconds)
+    @seconds = _seconds
+  end
+
+  def time_string
+    output_str = ""
+    _hours = @seconds / 3600
+    _minutes = (@seconds % 3600) / 60
+    _seconds = (@seconds % 3600) % 60
+
+    output_str << format_string(_hours)
+    output_str << ":"
+    output_str << format_string(_minutes)
+    output_str << ":"
+    output_str << format_string(_seconds)
+  end
+
+  def format_string(_data)
+    if _data >= 10
+      return _data.to_s
+    else
+      return "0" + _data.to_s
+    end
+  end
 end
