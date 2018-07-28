c2741b4adc5e405bbd059a214e312dfa9425669b
airi-14x <airi.14x@gmail.com>
2018-07-28 14:08:45 -0400

    Caesar Cipher Start

---
+++ caesar_cipher/caesar_cipher.rb
@@ -0,0 +1,12 @@
+def caesar_cipher(str, shift_index)
+  # Upper case - ASCII: 65 -> 90
+  # Lower case - ASCII: 97 -> 122
+
+  new_str = "" # New String with
+
+  # Getting character's ASCII .ord
+
+
+end
+
+caesar_cipher("What a string!", 5)
