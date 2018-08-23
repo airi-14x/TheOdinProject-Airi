bcd9e0508f68d5f3a6da6f216848d5a6241ebeed
airi-14x <airi.14x@gmail.com>
2018-08-23 15:07:55 -0400

    Need to work on : Creating an Edit Action & View

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,14 +1,19 @@
 class ArticlesController < ApplicationController
-
+  include ArticlesHelper
   def new
-    @article = Article.new
+    @article = Article.new(article_params)
   end
 
   def create
     #fail
-    @article = Article.new
-    @article.title = params[:article][:title]
+    #@article = Article.new
+    #@article.title = params[:article][:title]
+    #@article = Article.new(
+    #  title: params[:article][:title],
+    #body: params[:article][:body])
+    @article = Article.new(params[:article])
     @article.save
+    redirect_to article_path(@article)
   end
 
   def index
@@ -17,5 +22,11 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to action: "index"
   end
 end
